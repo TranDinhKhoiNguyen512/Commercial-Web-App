c68426e7dbfc3c3fb9cd4244fab83f6454da748b
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-05-23 10:24:10 +0700

    Added card api

--- server/Routes/card.js
+++ server/Routes/card.js
@@ -6,70 +6,98 @@ const dataPath = './Details/cards.json'
 
 // util functions 
 
-const saveAccountData = (data) => {
-    const stringifyData = JSON.stringify(data)
-    fs.writeFileSync(dataPath, stringifyData)
+const savecardData = (data) => {
+	const stringifyData = JSON.stringify(data)
+	fs.writeFileSync(dataPath, stringifyData)
 }
 
-const getAccountData = () => {
-    const jsonData = fs.readFileSync(dataPath)
-    return JSON.parse(jsonData)    
+const getcardData = () => {
+	const jsonData = fs.readFileSync(dataPath)
+	return JSON.parse(jsonData)    
 }
 
 
 // reading the data
 cardRoutes.get('/card', (req, res) => {
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
+	fs.readFile(dataPath, 'utf8', (err, data) => {
+	  if (err) {
+		throw err;
+	  }
 
-      res.send(JSON.parse(data));
-    });
+	  res.send(JSON.parse(data));
+	});
   });
 
-
-  cardRoutes.post('/card/add', (req, res) => {
+// Create - use post method
+cardRoutes.post('/card/add', (req, res) => {
    
-    var existAccounts = getAccountData()
-    const newAccountId = Math.floor(100000 + Math.random() * 900000)
+	var existcards = getcardData()
+	const newcardId = Math.floor(100000 + Math.random() * 900000)
    
-    existAccounts[newAccountId] = req.body
-     
-    console.log(existAccounts);
+	existcards[newcardId] = req.body
+	 
+	console.log(existcards);
 
-    saveAccountData(existAccounts);
-    res.send({success: true, msg: 'account data added successfully'})
+	savecardData(existcards);
+	res.send({success: true, msg: 'card data added successfully'})
 })
 
 // Read - get all cards from the json file
 cardRoutes.get('/card/getAll', (req, res) => {
-  const accounts = getAccountData()
-  res.send(accounts)
+  const cards = getcardData()
+  res.send(cards)
 })
 
+// Read - get cards by id
+cardRoutes.get('/card/getByID/:id', (req, res) => {
+	console.log('cards.getByID')
+  const cards = getcardData()
+  var id = req.params['id']
+  
+  //cards.length
+  for (var i = 0; i < cards.length; i++) {
+  	if(cards[i]['id'] == id){
+  		var result = cards[i]
+  		//console.log(cards[i])
+  		
+  	}
+  };
+  console.log('cards.getByID.Done')
+  res.send(result)
+})
+//get card by attributes
+cardRoutes.get('/card/get/:param1/:param2', (req, res) => {
+	console.log('cards.get')
+  const cards = getcardData()
+  var param1 = req.params['param1']
+  var param2 = req.params['param2']
+  //console.log(param1,param2)
+
+  console.log('cards.get.Done')
+  res.send(cards[0][param1][param2])
+})
 // Update - using Put method
 cardRoutes.put('/card/:id', (req, res) => {
-   var existAccounts = getAccountData()
+   var existcards = getcardData()
    fs.readFile(dataPath, 'utf8', (err, data) => {
-    const accountId = req.params['id'];
-    existAccounts[accountId] = req.body;
+	const cardId = req.params['id'];
+	existcards[cardId] = req.body;
 
-    saveAccountData(existAccounts);
-    res.send(`accounts with id ${accountId} has been updated`)
+	savecardData(existcards);
+	res.send(`cards with id ${cardId} has been updated`)
   }, true);
 });
 
 //delete - using delete method
 cardRoutes.delete('/card/delete/:id', (req, res) => {
    fs.readFile(dataPath, 'utf8', (err, data) => {
-    var existAccounts = getAccountData()
+	var existcards = getcardData()
 
-    const cardId = req.params['id'];
+	const cardId = req.params['id'];
 
-    delete existAccounts[userId];  
-    saveAccountData(existAccounts);
-    res.send(`accounts with id ${userId} has been deleted`)
+	delete existcards[userId];  
+	savecardData(existcards);
+	res.send(`cards with id ${userId} has been deleted`)
   }, true);
 })
 module.exports = cardRoutes;
