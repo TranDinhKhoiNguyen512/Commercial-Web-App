6907a004b57c2be25ea2bb6e61b136d2d944be2c
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-06-01 13:19:19 +0700

    Added card detail login

--- Primary Test/js/getInfor.js
+++ Primary Test/js/getInfor.js
@@ -1,5 +1,5 @@
 const api_url = "http://localhost:5000/card/get/swsh9-56";
-
+//sm5-151
 async function getISS() {
     const response = await fetch(api_url);
     const data = await response.json();
@@ -16,17 +16,43 @@ async function getISS() {
     document.getElementById('releaseDate').textContent = data.set.releaseDate;
     document.getElementById('id').textContent = data.id;
 
-    document.getElementById('price').textContent = "$" + data.cardmarket.prices.averageSellPrice;
+    
     document.getElementById('rarity').textContent = data.number + " / "  + data.rarity;
-    document.getElementById('types').textContent = data.types + " / "  + data.hp;
+    if(data.types == null){
+        typesText = "N/A"
+    }
+    else{
+        typesText = data.types
+    }
+    if(data.hp == null){
+        hpText = "N/A"
+    }
+    else{
+        hpText = data.hp
+    }
+    document.getElementById('types').textContent = typesText + " / "  + hpText;
+    document.getElementById('supertype').textContent = data.supertype + " / "  + data.subtypes;
     document.getElementById('base').textContent = data.set.series + " - " + data.set.id;
-    releaseDate
-    document.getElementById('stage').textContent = data.subtypes;
 
     
 
-    
-    document.getElementById('card_text').textContent = data.flavorText;
+    if(data.supertype == "Pokemon"){
+        cardText = data.flavorText;
+    }
+    else if(data.supertype == "Trainer"){
+        cardText = data.rules
+    }
+    if(cardText == null || cardText == ''){
+        cardText = "N/A";
+    }
+    if(data.cardmarket.prices.averageSellPrice == null){
+        priceText = "N/A";
+    }
+    else{
+        priceText = "$" + data.cardmarket.prices.averageSellPrice;
+    }
+    document.getElementById('price').textContent = priceText;
+    document.getElementById('card_text').textContent = cardText;
 
 
     
