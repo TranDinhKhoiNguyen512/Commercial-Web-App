4effe7e673512e5c7cabc2d28a2091739ca69939
=Nguyen Tran <=>
2022-06-06 21:22:36 +0700

    Update itemList.js

--- FinalProjectWAD/js/itemList.js
+++ FinalProjectWAD/js/itemList.js
@@ -1,35 +1,35 @@
 // Constant URL
 
 async function getCardByQuery(url) {
-    var api_url1 = url;
-    const response = await fetch(api_url1);
-    const pageData = await response.json();
-    var data = pageData.data
-    // Create Element and add Img from json
-    var counter = 1
-    // 
-	var pagingbutton = document.getElementById("paging__button");
-	if (data.length >= 1){
-		for(let i = 0; i <data.length; i++){
-	
+	var api_url1 = url;
+	const response = await fetch(api_url1);
+	const pageData = await response.json();
+	var data = pageData.data
+	// Create Element and add Img from json
+	var counter = 1
+	// 
+	var pagingButton = document.getElementById("paging__button");
+	if (data.length >= 1) {
+		for (let i = 0; i < data.length; i++) {
+
 			var str = `<div class="cover atvImg" onclick="location.href = '/productDetail.html?card=${data[i].id}';" ><div class="atvImg-layer" data-img="${data[i].images.small}"><a href="/productDetail.html"></a></div></div>`
-			document.getElementById('productList').insertAdjacentHTML( 'beforeend', str );
+			document.getElementById('productList').insertAdjacentHTML('beforeend', str);
 			counter = counter + 1
-			pagingbutton.style.display = "block";
+			pagingButton.style.display = "block";
 
 		}
 	}
 	//###STYLE###
-	else{
-		var str=`<h1>No Item Found</h1>`
-		document.getElementById('productList').insertAdjacentHTML( 'beforeend', str );
+	else {
+		var str = `<h1>No Item Found</h1>`
+		document.getElementById('productList').insertAdjacentHTML('beforeend', str);
 		console.log('Hmm')
-		pagingbutton.style.display = "none";
+		pagingButton.style.display = "none";
 	}
-    
+
 }
 
-async function atvImg(url){
+async function atvImg(url) {
 	await getCardByQuery(url);
 	var d = document,
 		de = d.documentElement,
@@ -40,43 +40,43 @@ async function atvImg(url){
 		totalImgs = imgs.length,
 		supportsTouch = 'ontouchstart' in win || navigator.msMaxTouchPoints;
 
-	if(totalImgs <= 0){
+	if (totalImgs <= 0) {
 		return;
 	}
 
-	for(var l=0;l<totalImgs;l++){
+	for (var l = 0; l < totalImgs; l++) {
 
 		var thisImg = imgs[l],
 			layerElems = thisImg.querySelectorAll('.atvImg-layer'),
 			totalLayerElems = layerElems.length;
 
-		if(totalLayerElems <= 0){
+		if (totalLayerElems <= 0) {
 			continue;
 		}
 
-		while(thisImg.firstChild) {
+		while (thisImg.firstChild) {
 			thisImg.removeChild(thisImg.firstChild);
 		}
-	
+
 		var containerHTML = d.createElement('div'),
 			shineHTML = d.createElement('div'),
 			shadowHTML = d.createElement('div'),
 			layersHTML = d.createElement('div'),
 			layers = [];
 
-		thisImg.id = 'atvImg__'+l;
+		thisImg.id = 'atvImg__' + l;
 		containerHTML.className = 'atvImg-container';
 		shineHTML.className = 'atvImg-shine';
 		shadowHTML.className = 'atvImg-shadow';
 		layersHTML.className = 'atvImg-layers';
 
-		for(var i=0;i<totalLayerElems;i++){
+		for (var i = 0; i < totalLayerElems; i++) {
 			var layer = d.createElement('div'),
 				imgSrc = layerElems[i].getAttribute('data-img');
 
 			layer.className = 'atvImg-rendered-layer';
-			layer.setAttribute('data-layer',i);
-			layer.style.backgroundImage = 'url('+imgSrc+')';
+			layer.setAttribute('data-layer', i);
+			layer.style.backgroundImage = 'url(' + imgSrc + ')';
 			layersHTML.appendChild(layer);
 
 			layers.push(layer);
@@ -88,58 +88,58 @@ async function atvImg(url){
 		thisImg.appendChild(containerHTML);
 
 		var w = thisImg.clientWidth || thisImg.offsetWidth || thisImg.scrollWidth;
-		thisImg.style.transform = 'perspective('+ w*3 +'px)';
+		thisImg.style.transform = 'perspective(' + w * 3 + 'px)';
 
-		if(supportsTouch){
+		if (supportsTouch) {
 			win.preventScroll = false;
 
-	        (function(_thisImg,_layers,_totalLayers,_shine) {
-				thisImg.addEventListener('touchmove', function(e){
-					if (win.preventScroll){
+			(function (_thisImg, _layers, _totalLayers, _shine) {
+				thisImg.addEventListener('touchmove', function (e) {
+					if (win.preventScroll) {
 						e.preventDefault();
 					}
-					processMovement(e,true,_thisImg,_layers,_totalLayers,_shine);		
+					processMovement(e, true, _thisImg, _layers, _totalLayers, _shine);
 				});
-	            thisImg.addEventListener('touchstart', function(e){
-	            	win.preventScroll = true;
-					processEnter(e,_thisImg);		
+				thisImg.addEventListener('touchstart', function (e) {
+					win.preventScroll = true;
+					processEnter(e, _thisImg);
 				});
-				thisImg.addEventListener('touchend', function(e){
+				thisImg.addEventListener('touchend', function (e) {
 					win.preventScroll = false;
-					processExit(e,_thisImg,_layers,_totalLayers,_shine);		
+					processExit(e, _thisImg, _layers, _totalLayers, _shine);
 				});
-	        })(thisImg,layers,totalLayerElems,shineHTML);
-	    } else {
-	    	(function(_thisImg,_layers,_totalLayers,_shine) {
-				thisImg.addEventListener('mousemove', function(e){
-					processMovement(e,false,_thisImg,_layers,_totalLayers,_shine);		
+			})(thisImg, layers, totalLayerElems, shineHTML);
+		} else {
+			(function (_thisImg, _layers, _totalLayers, _shine) {
+				thisImg.addEventListener('mousemove', function (e) {
+					processMovement(e, false, _thisImg, _layers, _totalLayers, _shine);
 				});
-	            thisImg.addEventListener('mouseenter', function(e){
-					processEnter(e,_thisImg);		
+				thisImg.addEventListener('mouseenter', function (e) {
+					processEnter(e, _thisImg);
 				});
-				thisImg.addEventListener('mouseleave', function(e){
-					processExit(e,_thisImg,_layers,_totalLayers,_shine);		
+				thisImg.addEventListener('mouseleave', function (e) {
+					processExit(e, _thisImg, _layers, _totalLayers, _shine);
 				});
-	        })(thisImg,layers,totalLayerElems,shineHTML);
-	    }
+			})(thisImg, layers, totalLayerElems, shineHTML);
+		}
 	}
 
-	function processMovement(e, touchEnabled, elem, layers, totalLayers, shine){
+	function processMovement(e, touchEnabled, elem, layers, totalLayers, shine) {
 
 		var bdst = bd.scrollTop || htm.scrollTop,
 			bdsl = bd.scrollLeft,
-			pageX = (touchEnabled)? e.touches[0].pageX : e.pageX,
-			pageY = (touchEnabled)? e.touches[0].pageY : e.pageY,
+			pageX = (touchEnabled) ? e.touches[0].pageX : e.pageX,
+			pageY = (touchEnabled) ? e.touches[0].pageY : e.pageY,
 			offsets = elem.getBoundingClientRect(),
 			w = elem.clientWidth || elem.offsetWidth || elem.scrollWidth,
 			h = elem.clientHeight || elem.offsetHeight || elem.scrollHeight,
-			wMultiple = 320/w,
-			offsetX = 0.52 - (pageX - offsets.left - bdsl)/w,
-			offsetY = 0.52 - (pageY - offsets.top - bdst)/h,
+			wMultiple = 320 / w,
+			offsetX = 0.52 - (pageX - offsets.left - bdsl) / w,
+			offsetY = 0.52 - (pageY - offsets.top - bdst) / h,
 			dy = (pageY - offsets.top - bdst) - h / 2,
 			dx = (pageX - offsets.left - bdsl) - w / 2,
-			yRotate = (offsetX - dx)*(0.07 * wMultiple),
-			xRotate = (dy - offsetY)*(0.1 * wMultiple),
+			yRotate = (offsetX - dx) * (0.07 * wMultiple),
+			xRotate = (dy - offsetY) * (0.1 * wMultiple),
 			imgCSS = 'rotateX(' + xRotate + 'deg) rotateY(' + yRotate + 'deg)',
 			arad = Math.atan2(dy, dx),
 			angle = arad * 180 / Math.PI - 90;
@@ -148,38 +148,37 @@ async function atvImg(url){
 			angle = angle + 360;
 		}
 
-		if(elem.firstChild.className.indexOf(' over') != -1){
+		if (elem.firstChild.className.indexOf(' over') != -1) {
 			imgCSS += ' scale3d(1.07,1.07,1.07)';
 		}
 		elem.firstChild.style.transform = imgCSS;
-		
-		shine.style.background = 'linear-gradient(' + angle + 'deg, rgba(255,255,255,' + (pageY - offsets.top - bdst)/h * 0.4 + ') 0%,rgba(255,255,255,0) 80%)';
-		shine.style.transform = 'translateX(' + (offsetX * totalLayers) - 0.1 + 'px) translateY(' + (offsetY * totalLayers) - 0.1 + 'px)';	
+
+		shine.style.background = 'linear-gradient(' + angle + 'deg, rgba(255,255,255,' + (pageY - offsets.top - bdst) / h * 0.4 + ') 0%,rgba(255,255,255,0) 80%)';
+		shine.style.transform = 'translateX(' + (offsetX * totalLayers) - 0.1 + 'px) translateY(' + (offsetY * totalLayers) - 0.1 + 'px)';
 
 		var revNum = totalLayers;
-		for(var ly=0;ly<totalLayers;ly++){
+		for (var ly = 0; ly < totalLayers; ly++) {
 			layers[ly].style.transform = 'translateX(' + (offsetX * revNum) * ((ly * 2.5) / wMultiple) + 'px) translateY(' + (offsetY * totalLayers) * ((ly * 2.5) / wMultiple) + 'px)';
 			revNum--;
 		}
 	}
 
-	function processEnter(e, elem){
+	function processEnter(e, elem) {
 		elem.firstChild.className += ' over';
 	}
 
-	function processExit(e, elem, layers, totalLayers, shine){
+	function processExit(e, elem, layers, totalLayers, shine) {
 
 		var container = elem.firstChild;
 
-		container.className = container.className.replace(' over','');
+		container.className = container.className.replace(' over', '');
 		container.style.transform = '';
 		shine.style.cssText = '';
-		
-		for(var ly=0;ly<totalLayers;ly++){
+
+		for (var ly = 0; ly < totalLayers; ly++) {
 			layers[ly].style.transform = '';
 		}
 
 	}
-
 }
 
